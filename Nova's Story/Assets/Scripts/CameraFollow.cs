de2fa9b4a7785509c28a328e61cdbdf227e1e9a7
Preston <pfhgetty1337@gmail.com>
2017-11-18 22:35:03 -0800

    Revert "Revert "Camera Following + Ground Texture""
    This reverts commit 9dedf279cea3eca12f7fd5c5a7c753b904088de0.

---
+++ Nova's Story/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    [SerializeField] private Player player;
+    private Camera cam;
+    [SerializeField] private float maxDistance;
+	// Use this for initialization
+	void Start () {
+        cam = this.GetComponent<Camera>();
+	}
+	
+	// Use FixedUpdate to synchronize with player movement
+	void FixedUpdate () {
+        Vector3 targetPosition = (cam.ScreenToWorldPoint(Input.mousePosition) + (player.transform.position) * 1.5f) / 2.5f;
+        targetPosition.z = -10;
+        this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, 0.2f);
+    }
+}
