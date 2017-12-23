0217f45519f839894527b4586ba26bc658e054a2
ZillionImp <33796230+ZillionImp@users.noreply.github.com>
2017-12-22 21:19:43 -0800

    Dialogue scripting
    Added a script to go through dialogue by clickling space

---
+++ Nova's Story/Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour {
+
+    [SerializeField] private GameObject textBox;
+    [SerializeField] private Text theText;
+    [SerializeField] private TextAsset[] textFile;
+    [SerializeField] private string[] textLines;
+
+    [SerializeField] private int currentLine;
+    [SerializeField] private int endAtLine;
+    [SerializeField] private int textFileCounter;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        if (textFile[textFileCounter] != null)
+        {
+
+            textLines = (textFile[textFileCounter].text.Split('\n'));
+            theText.text = textLines[0];
+
+        }
+
+        else if (textFile[textFileCounter] == null)
+        {
+
+            textBox.gameObject.SetActive(false);
+
+        }
+
+        if (endAtLine == 0)
+        {
+
+            endAtLine = textLines.Length - 1;
+
+        }
+
+    }
+
+    void Update()
+    {
+
+        while (currentLine != endAtLine)
+        {
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+
+                currentLine++;
+                theText.text = textLines[currentLine];
+
+            }
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+
+            textFileCounter++;
+            this.Start();
+
+        }
+
+    }
+
+}
---
+++ Nova's Story/Assets/Scripts/TextImporter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextImporter : MonoBehaviour {
+
+    [SerializeField] private TextAsset[] textFile;
+    [SerializeField] private string[] textLines;
+
+	// Use this for initialization
+	void Start () {
+	    
+        if (textFile[0] != null)
+        {
+
+            textLines = (textFile[0].text.Split('\n'));
+
+        }
+
+	}
+	
+}
