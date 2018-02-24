6a0aa41244e84fcae049b27ff1778fb531553269
ZillionImp <33796230+ZillionImp@users.noreply.github.com>
2018-02-23 20:29:26 -0800

    Cloak
    We added a working cl0aking field ability

---
+++ Nova's Story/Assets/Scripts/Cloaking_Field.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloaking_Field : MonoBehaviour {
+
+    private SpriteRenderer rend;
+    [SerializeField] private float rate;
+    private Health health;
+    private float damage;
+
+	// Use this for initialization
+	void Start () {
+        rend = this.gameObject.GetComponent<SpriteRenderer>();
+        health = this.gameObject.GetComponent<Health>();
+        
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (rend.enabled == false)
+        {
+
+            damage += rate * Time.deltaTime;
+            health.TakeDamage((int)damage);
+            damage -= (int)damage;
+
+        }
+        if ((this.health.GetShield() > 0 ) && (Input.GetKeyUp(KeyCode.R)))
+        {
+
+            rend.enabled = !rend.enabled;
+            
+            
+
+        }
+        if (this.health.GetShield() <= 0)
+        {
+
+            rend.enabled = true;
+
+        }
+
+	}
+}
