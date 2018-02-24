8453e7b8fee00102d250c8d0810edb00d3208976
jesusrice <33796267+jesusrice@users.noreply.github.com>
2018-02-23 19:54:52 -0800

    fixed moving platforms
    for juice

--- Nova's Story/Assets/Scripts/MovingPlatform.cs
+++ Nova's Story/Assets/Scripts/MovingPlatform.cs
@@ -6,7 +6,7 @@ public class MovingPlatform : MonoBehaviour {
 	[SerializeField] private Transform handle1;
 	[SerializeField] private Transform handle2;
 	[SerializeField] private GameObject platform; 
-	[SerializeField] private float speed;
+	[SerializeField] private float speed = .1f;
 	private float time;
 
 	private bool directionSwitch;
@@ -20,18 +20,32 @@ public class MovingPlatform : MonoBehaviour {
 
 	void FixedUpdate () {
 		
-		time += Time.deltaTime;
+		time += Time.deltaTime*speed;
 
 		if (time > 1) {
 			time = 0;
 			directionSwitch = !directionSwitch;
 		}
-		//speed = time / ;
 		if (directionSwitch) {
-			pos = Vector2.Lerp(handle1.position, handle2.position, time/2f);
+			pos = Vector2.Lerp(handle1.position, handle2.position, time);
 		} else {
-			pos = Vector2.Lerp(handle2.position, handle1.position, time/2f);
+			pos = Vector2.Lerp(handle2.position, handle1.position, time);
 		}
 		platform.transform.position = pos;
 	}
+//	public float rightLimit = 2.5f;
+//	public float leftLimit = 1.0f;
+//	public float speed = 1.0f;
+//	private int direction = 1;
+//
+//	void Update () {
+//		if (transform.position.x > rightLimit) {
+//			direction = -1;
+//		}
+//		else if (transform.position.x < leftLimit) {
+//			direction = 1;
+//		}
+//		movement = Vector3.right * direction * speed * Time.deltaTime; 
+//		transform.Translate(movement); 
+//	}
 }
